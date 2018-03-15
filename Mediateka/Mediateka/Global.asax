eb0ad07c5b0394c9665e0def62b823fbec2935c6
LukDro <lukdro7001@gmail.com>
2018-03-15 19:36:21 +0100

    new commit

---
+++ Mediateka/Mediateka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mediateka.MvcApplication" Language="C#" %>
---
+++ Mediateka/Mediateka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mediateka.Startup))]
+namespace Mediateka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
